b6a98609de0c4bb3eda26380d7e5d801a35f6ad7
kobione <getkobi@gmail.com>
2016-11-10 23:54:01 -0800

    update with Derrick + notes.

--- src/vuex/actions.js
+++ src/vuex/actions.js
@@ -1,5 +1,13 @@
 import { ADD_NOTE, DELETE_NOTE } from './constants';
 
+// commit given to us by the context object, given to us by Vuex
+  // so by default, Vuex know how to handle the action we defined, by passing in the
+  // context object with properties that would allow the use of Vuex inside our actions
+  // eg, commit (or "disptach"), which allows us to commit/dispatch our mutation
+  // Other properties as well, which helps us interact with the store (other than commit/dispatch).
+
+// "event" is given to us as part of Javascript WHEN IN THE DOM
+// Thus, it is a DOM EVENT
 export const addNote = ({ commit }, event ) => {
   // Get data from the input element using the DOM keyboard enter event.
 
@@ -8,13 +16,14 @@ export const addNote = ({ commit }, event ) => {
   // key on the keyboard and pulls the data from the event object
 
   // "event" and "event.target" are built into Javascript
-  const newNote = event.target.value;
+  // Accessing the <input>s value attribute (b/c we are using this inside <input>)
+  const newNoteText = event.target.value;
 
-  // newNote.trim() simply trims off any white spaces at the start and end of
+  // newNoteText.trim() simply trims off any white spaces at the start and end of
   // the input value just to make sure the user doesn't submit empty spaces or empty
   // strings
-  if (newNote.trim())
-    commit(ADD_NOTE, newNote);
+  if (newNoteText.trim())
+    commit(ADD_NOTE, newNoteText);
 
   // Line 18 resets the input element to blank so that the user can continue
   // adding some more notes without having to manually delete the value they had
--- src/vuex/mutations.js
+++ src/vuex/mutations.js
@@ -1,12 +1,14 @@
 import { ADD_NOTE, DELETE_NOTE } from './constants';
 
 export const mutations = {
-  [ADD_NOTE] (state, theNewNote) {
+  // in Actions, we passed in the newNoteText as a "payload" (payload is always 2nd argument, 1st is state).
+  // Calling local variable, newNoteText, but we passed in newNoteText in actions.
+  [ADD_NOTE] (state, newNoteText) {
     const newNote = {
       // state.notes.length gets the number of elements in the notes array
       id: state.notes.length + 1, // we're incrementing the notes IDS automatically
       // we're assigning our new note value to the text property
-      text: theNewNote,
+      text: newNoteText,
       favorite: false
     };
 
