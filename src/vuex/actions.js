25d70c130f601bdc276125936254cb9906890c6e
kobione <getkobi@gmail.com>
2016-11-18 12:03:33 -0800

    Add validation (first part): if user submits an empty string, it will return immediatly, thus not executing the rest of the code (aka, will not allow new imput entry to work.)

--- src/vuex/actions.js
+++ src/vuex/actions.js
@@ -3,9 +3,24 @@ import { ADD_NOTE, DELETE_NOTE } from './constants';
 // using the addNote mutation, from mutations.js
 export const addNote = ({ commit }) => {
 
+  // validating text and body not empty
+  const newText = document.getElementById('text').value
+  if (newText.trim().length == 0) {
+    // if we return nothing, it will exit, will simply stop running
+    return;
+  }
+
+  const newBody = document.getElementById('body').value
+  if (newBody.trim().length == 0) {
+    return;
+  }
+
+  // This is a new object, not a function:
+  // Don't have to pass in vars, b/c we are inside the same addNote function
+  // newText & newBody are local variables to the addNote function
   const newNoteObj = {
-    text: document.getElementById('text').value,
-    body: document.getElementById('body').value
+    text: newText,
+    body: newBody
   };
 
   console.log(newNoteObj);
